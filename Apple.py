ac7112c3db17d19bac2eb72bcfc3fba9923cf833
Aristoteles Bernardo <aristoteles.bernardo.elc@gmail.com>
2021-08-09 16:58:41 +0100

    Teste 1.0

--- Apple.py
+++ Apple.py
@@ -1,20 +1,19 @@
-#%%
-msg = "hello world"
-print (msg)
-
-
-import matplotlib.pyplot as plt
-import numpy as nump
-x = [3,4,6,3,4,3,8,9,7,8,]    # Create a list of numbers
-plt.plot(x)       # Plot the sine of each x point
-plt.show() 
-
-
-
-#%%
-import matplotlib.pyplot as plt
+# %%
 import numpy as np
+import numpy as nump
+import matplotlib.pyplot as plt
+msg = "hello world "
+print(msg)
 
-x = np.linspace(0, 20, 100)  # Create a list of evenly-spaced numbers over the range
+
+x = [3, 4, 6, 3, 4, 3, 8, 9, 7, 8, ]    # Create a list of numbers
+plt.plot(x)       # Plot the sine of each x point
+plt.show()
+
+
+# %%
+
+# Create a list of evenly-spaced numbers over the range
+x = np.linspace(0, 20, 100)
 plt.plot(x, np.sin(x))       # Plot the sine of each x point
 plt.show()                   # Display the plot
